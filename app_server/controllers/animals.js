36b8817452b1c441adabd41942ad26fe1bad4fa7
Curtis Spendlove <curtis@knightoftheoldcode.com>
2017-08-27 14:41:25 -0600

    Use Parent Layout

--- app_server/controllers/animals.js
+++ app_server/controllers/animals.js
@@ -1,9 +1,9 @@
 const index = function (req, res) {
-  res.render('index', { title: 'Animals - List' });
+  res.render('animals/index', { title: 'Animals - List' });
 };
 
 const show = function (req, res) {
-  res.render('index', { title: 'Animals - Show' });
+  res.render('animals/index', { title: 'Animals - Show' });
 };
 
 module.exports = {
