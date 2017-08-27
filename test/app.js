bb8d80db37df0e4f6d8c6f53367a00c343db9a1d
Curtis Spendlove <curtis@knightoftheoldcode.com>
2017-08-27 00:31:51 -0600

    Add Mocha and Configure Code Coverage

---
+++ test/app.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+
+describe('App', function() {
+  describe('hard-coded assert', function() {
+    it('should assert true', function() {
+      assert.equal(1, 1);
+    });
+  });
+});
+
